10a7862bc9d6e2682eb8d2628eb03fdb03a48d5e
Soumya Deb <soumyadeb1809@gmail.com>
2021-06-13 17:46:48 +0530

    Add DtoFactory

--- src/main/java/in/soumyadeb/shortify/util/DtoFactory.java
+++ src/main/java/in/soumyadeb/shortify/util/DtoFactory.java
@@ -1,4 +1,43 @@
 package in.soumyadeb.shortify.util;
 
+import in.soumyadeb.shortify.dto.ShortUrlDto;
+import in.soumyadeb.shortify.dto.UserDto;
+import in.soumyadeb.shortify.entity.ShortUrl;
+import in.soumyadeb.shortify.entity.User;
+
 public class DtoFactory {
+
+    /**
+     * Factory method to create UserDto.
+     *
+     * @param user entity
+     * @return instance of UserDto
+     */
+    public UserDto createUserDto(User user) {
+        UserDto userDto = new UserDto();
+        userDto.setId(user.getId());
+        userDto.setFirstName(user.getFirstName());
+        userDto.setLastName(user.getLastName());
+        userDto.setEmail(user.getEmail());
+
+        return userDto;
+    }
+
+    /**
+     * Factory method to create ShortUrlDto.
+     *
+     * @param shortUrl entity
+     * @param userId user Id
+     * @return instance of UserDto
+     */
+    public ShortUrlDto createShortUrlDto(ShortUrl shortUrl, Integer userId) {
+        ShortUrlDto shortUrlDto = new ShortUrlDto();
+        shortUrlDto.setId(shortUrl.getId());
+        shortUrlDto.setTitle(shortUrl.getTitle());
+        shortUrlDto.setShortName(shortUrl.getShortName());
+        shortUrlDto.setLongUrl(shortUrl.getLongUrl());
+        shortUrlDto.setUserId(userId);
+
+        return shortUrlDto;
+    }
 }
