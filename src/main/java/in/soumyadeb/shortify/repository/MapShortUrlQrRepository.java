9e55e7edcba81490d1206c9bc3a2327d67dbac3f
psycho-valker <rameez.connect@gmail.com>
2023-07-27 18:18:08 +0530

    Aded config code

--- src/main/java/in/soumyadeb/shortify/repository/MapShortUrlQrRepository.java
+++ src/main/java/in/soumyadeb/shortify/repository/MapShortUrlQrRepository.java
@@ -1,10 +1,13 @@
 package in.soumyadeb.shortify.repository;
 
 import in.soumyadeb.shortify.entity.MapShortUrlQr;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-public interface MapShortUrlQrRepository {
+@Repository
+public interface MapShortUrlQrRepository extends JpaRepository<MapShortUrlQr, Integer> {
 
     @Query("FROM MapShortUrlQr WHERE shortUrlId = :shortUrlId")
     MapShortUrlQr findByShortUrlId(
